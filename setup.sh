1bd94577a34120b55040bda4d3d245afa5fa1609
clickbazaarnow <sramoji@7831c1c380aa.ant.amazon.com>
2014-09-01 13:00:59 -0700

    Adding logging, error handling and clustering

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 if [ ! -d "build" ]; then
 	mkdir -p build/node-root
+	mkdir -p build/output/logs
+fi
+
+if [ ! -f "build/output/logs/cbn.log" ]; then
+	echo "log file doen't exist, creating one"
+	touch build/output/logs/cbn.log
 fi
 cp ./src/node/* build/node-root
 node ./build/node-root/cbn-server.js
--- src/node/cbn-server.js
+++ src/node/cbn-server.js
@@ -1,8 +1,46 @@
 var express = require('express');
-var app = express();
+var bunyan = require('bunyan');
+var cluster = require('cluster');
+var connectDomain = require('connect-domain');
+var fs = require('fs');
 
-app.get('/', function(req, res){
-  res.send('hello world');
+var NUM_WORKERS = 1;
+
+var app = express();
+var log = bunyan.createLogger({
+    name: 'cbn',
+    streams: [{
+        type: 'rotating-file',
+        path: 'build/output/logs/cbn.log',
+        period: '1d',   // daily rotation
+		level: bunyan.DEBUG
+    }]
+});
+log.on('error', function (err, stream) {
+    console.log("Got error from logger : " + err);
 });
 
-app.listen(3000);
+if (cluster.isMaster) {
+	// Fork workers.
+	for (var i = 0; i < NUM_WORKERS; i++) {
+		cluster.fork();
+  	}
+
+	cluster.on('exit', function(worker, code, signal) {
+    	log.error('worker ' + worker.process.pid + ' died');
+  	});
+} else {
+	app.listen(3000);
+}
+
+app.use(connectDomain())
+	.get('/', function(req, res){
+		throw new Error("some error was thrown");
+		res.send('hello world');
+		log.info("sent reponse");
+	})
+	.use(function(err, req, res, next) {
+		log.error("Got error : " + err.message);
+		res.send("Got an unexpected error in server");
+	});
+
